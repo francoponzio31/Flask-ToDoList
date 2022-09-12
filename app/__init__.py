0273f832c293e53595346acb2c6416b0b143dca4
fponzio <fponzio@leafnoise.io>
2022-09-12 00:42:40 -0300

    secret key update

--- app/__init__.py
+++ app/__init__.py
@@ -3,14 +3,12 @@ from flask_bootstrap import Bootstrap
 from .blueprints.auth import auth, login_manager
 from .blueprints.todo_bp import todo_bp
 from .db_connection import connect_to_db
-import secrets
 
 def create_app():
 
     app = Flask(__name__, template_folder="../templates", static_folder="../static")
 
-    secret_key = secrets.token_hex(16)
-    app.config['SECRET_KEY'] = secret_key
+    app.config['SECRET_KEY'] = "637212dc2c114f0af5da386ee1a4f66e"
  
     app.register_blueprint(todo_bp)
     app.register_blueprint(auth)
